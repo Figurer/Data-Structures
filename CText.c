b03797ef596e6cbbf32001c1af7067a4d64f7773
Figurer <1421138622@qq.com>
2017-07-22 09:58:50 +0800

    selftest01
    Input:
    19 *
    
    Output:
    *****
     ***
      *
     ***
    *****
    2

---
+++ CText.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+
+int GetLine(int n);
+void Print(int line, char mark);
+
+int main()
+{
+    char mark;
+    int n;
+    scanf("%d %c", &n, &mark);
+    int line = GetLine(n);
+    Print(line, mark);
+    printf("%d", n - (6 * line + line*(line-1)*4 / 2) -1);
+    return 0;
+}
+
+int GetLine(int n)
+{
+    int temp = 2;
+    int number = 1;
+    int line = 0;
+    while (number <= n)
+    {
+        line++;
+        temp += 4;
+        number += temp;
+    }
+    line--;
+    return line;
+}
+
+ void Print(int line, char mark)
+{
+    int cnt;
+    for (int i = -line; i <= line; i++)
+    {
+        cnt = -abs(i) + line;
+
+        for (int i = 0; i < cnt; i++)
+            printf(" ");
+
+        for (int j = 1; j <= 2 * abs(i) + 1; j++)
+        {
+            printf("%c", mark);
+        }
+
+        printf("\n");
+    }
+}
